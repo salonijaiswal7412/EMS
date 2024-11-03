19e62c51cdb515653bd77f48b03936ddccaf922c
saloni <salonijaiswal.7412@gmail.com>
2024-11-03 15:58:08 +0530

    created admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Login from './Components/Auth/Login'
 import EmplDashboard from './Components/Dashboard/EmplDashboard'
+import AdmDashboard from './Components/Dashboard/AdmDashboard'
 
 function App() {
   return (
     <>
     {/* <Login/> */}
-    <EmplDashboard />
+    {/* <EmplDashboard /> */}
+    <AdmDashboard/>
     </>
   )
 }
--- src/Components/TaskList/TaskList.jsx
+++ src/Components/TaskList/TaskList.jsx
@@ -3,10 +3,47 @@ import React from 'react'
 const TaskList = () => {
   return (
     <div id="tasklist" className='h-[41vh] mt-10 py-5 w-full flex items-center gap-5 flex-nowrap overflow-x-auto'>
-        <div className="flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl">
-            <div>
-                <h3 className='bg-red-600 text-white w-10'>High</h3>
+        <div className="flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl overflow-hidden">
+            <div className='flex justify-between bg-red-400 p-5 items-center text-sm'>
+                <h3 className='bg-red-600 text-white  px-3 py-1 '>High</h3>
+                <h4 className='bg-transparent'>16 Sep 2024</h4>
             </div>
+            <h2 className='bg-transparent text-2xl font-semibold m-3'>Make a Youtube video</h2>
+            <p className='bg-transparent m-3 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, ipsam suscipit ab iste nostrum ratione!</p>
+        </div>
+
+        <div className="flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl overflow-hidden">
+            <div className='flex justify-between bg-red-400 p-5 items-center text-sm'>
+                <h3 className='bg-red-600 text-white  px-3 py-1 '>High</h3>
+                <h4 className='bg-transparent'>16 Sep 2024</h4>
+            </div>
+            <h2 className='bg-transparent text-2xl font-semibold m-3'>Make a Youtube video</h2>
+            <p className='bg-transparent m-3 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, ipsam suscipit ab iste nostrum ratione!</p>
+        </div>
+
+        <div className="flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl overflow-hidden">
+            <div className='flex justify-between bg-red-400 p-5 items-center text-sm'>
+                <h3 className='bg-red-600 text-white  px-3 py-1 '>High</h3>
+                <h4 className='bg-transparent'>16 Sep 2024</h4>
+            </div>
+            <h2 className='bg-transparent text-2xl font-semibold m-3'>Make a Youtube video</h2>
+            <p className='bg-transparent m-3 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, ipsam suscipit ab iste nostrum ratione!</p>
+        </div>
+        <div className="flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl overflow-hidden">
+            <div className='flex justify-between bg-red-400 p-5 items-center text-sm'>
+                <h3 className='bg-red-600 text-white  px-3 py-1 '>High</h3>
+                <h4 className='bg-transparent'>16 Sep 2024</h4>
+            </div>
+            <h2 className='bg-transparent text-2xl font-semibold m-3'>Make a Youtube video</h2>
+            <p className='bg-transparent m-3 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, ipsam suscipit ab iste nostrum ratione!</p>
+        </div>
+        <div className="flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl overflow-hidden">
+            <div className='flex justify-between bg-red-400 p-5 items-center text-sm'>
+                <h3 className='bg-red-600 text-white  px-3 py-1 '>High</h3>
+                <h4 className='bg-transparent'>16 Sep 2024</h4>
+            </div>
+            <h2 className='bg-transparent text-2xl font-semibold m-3'>Make a Youtube video</h2>
+            <p className='bg-transparent m-3 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, ipsam suscipit ab iste nostrum ratione!</p>
         </div>
         
         
